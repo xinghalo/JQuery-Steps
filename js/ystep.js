4ef845aa92c8dce0ff9c77c04b665750047c7530
xinghalo <xinghalo@163.com>
2016-05-28 01:03:11 +0800

    修剪代码

---
+++ js/ystep.js
@@ -0,0 +1,147 @@
+(function($){
+  $.fn.extend({
+    //初始化
+    loadStep: function(params){
+      
+      //基础框架
+      var baseHtml =  "<div class='ystep-container'>"+
+                        "<ul class='ystep-container-steps'></ul>"+
+                        "<div class='ystep-progress'>"+
+                          "<p class='ystep-progress-bar'><span class='ystep-progress-highlight' style='width:0%'></span></p>"+
+                        "</div>"+
+                       
+                      "</div>";
+      //步骤框架
+      var stepHtml = "<li class='ystep-step ystep-step-undone'></li>";
+      var stepButtonHtml = "<div class='step-button'><button type='button' class='btn btn-default nextBtn'>上一步</button>"+
+                        "<button type='button' class='btn btn-default nextBtn'>下一步</button></div>";
+
+      //支持填充多个步骤容器
+      $(this).each(function(i,n){
+        var $baseHtml = $(baseHtml),
+        $stepHtml = $(stepHtml),
+        $ystepContainerSteps = $baseHtml.find(".ystep-container-steps"),
+        arrayLength = 0,
+        $n = $(n),
+        i=0;
+        
+        //步骤
+        arrayLength = params.steps.length;
+        for(i=0;i<arrayLength;i++){
+          var _s = params.steps[i];
+          //构造步骤html
+          $stepHtml.text(_s);
+          //将步骤插入到步骤列表中
+          $ystepContainerSteps.append($stepHtml);
+          //重置步骤
+          $stepHtml = $(stepHtml);
+        }
+
+        var $stepButtonHtml = $(stepButtonHtml); 
+        $ystepContainerSteps.append($stepButtonHtml);
+
+        var stepCount = arrayLength-1;
+        var containerWidth = (stepCount*200+200)+"px";
+        var progressWidth = (stepCount*200)+"px";
+        $baseHtml.css({
+          width: containerWidth,
+          left: "15%"
+        });
+        $baseHtml.find(".ystep-progress").css({
+          width: progressWidth
+        });
+        $baseHtml.find(".ystep-progress-bar").css({
+          width: progressWidth
+        });
+
+        $baseHtml.find(".step-button").css({
+          width:"200px",
+          left:"850px",
+          top:"-85px"
+        });
+        $baseHtml.addClass("ystep-lg"); 
+        $baseHtml.addClass("ystep-blue");
+        //插入到容器中
+        $n.append($baseHtml);
+        //默认执行第一个步骤
+        $n.setStep(1);
+      });
+    },
+    //跳转到指定步骤
+    setStep: function(step) {
+      $(this).each(function(i,n){
+        //获取当前容器下所有的步骤
+        var $steps = $(n).find(".ystep-container").find("li");
+        var $progress =$(n).find(".ystep-container").find(".ystep-progress-highlight");
+        //判断当前步骤是否在范围内
+        if(1<=step && step<=$steps.length){
+          //更新进度
+          var scale = "%";
+          scale = Math.round((step-1)*200/($steps.length-1))+scale;
+          $progress.animate({
+            width: scale
+          },{
+            speed: 1000,
+            done: function() {
+              //移动节点
+              $steps.each(function(j,m){
+                var _$m = $(m);
+                var _j = j+1;
+                if(_j < step){
+                  _$m.attr("class","ystep-step-done");
+                }else if(_j === step){
+                  _$m.attr("class","ystep-step-active");
+                }else if(_j > step){
+                  _$m.attr("class","ystep-step-undone");
+                }
+              });
+            }
+          });
+        }else{
+          return false;
+        }
+      });
+    },
+    //获取当前步骤
+    getStep: function() {
+      var result = [];
+      
+      $(this)._searchStep(function(i,j,n,m){
+        result.push(j+1);
+      });
+      
+      if(result.length == 1) {
+        return result[0];
+      }else{
+        return result;
+      }
+    },
+    //下一个步骤
+    nextStep: function() {
+      $(this)._searchStep(function(i,j,n,m){
+        $(n).setStep(j+2);
+      });
+    },
+    //上一个步骤
+    prevStep: function() {
+      $(this)._searchStep(function(i,j,n,m){
+        $(n).setStep(j);
+      });
+    },
+    //通用节点查找
+    _searchStep: function (callback) {
+      $(this).each(function(i,n){
+        var $steps = $(n).find(".ystep-container").find("li");
+        $steps.each(function(j,m){
+          //判断是否为活动步骤
+          if($(m).attr("class") === "ystep-step-active"){
+            if(callback){
+              callback(i,j,n,m);
+            }
+            return false;
+          }
+        });
+      });
+    }
+  });
+})(jQuery);
